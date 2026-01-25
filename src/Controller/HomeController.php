4f86a7a2f5a7f74fd929e68d4a8092bca988e709
NoheilaRamdani <noheila.ramdani@etudiant.univ-reims.fr>
2026-01-25 15:55:40 +0100

    Landing page + reorganisation routes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ScoreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(ScoreRepository $scoreRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $topScores = $scoreRepository->findBy([], ['score' => 'DESC'], 3);
+
+        return $this->render('home/index.html.twig', [
+            'topScores' => $topScores,
+        ]);
     }
 }
